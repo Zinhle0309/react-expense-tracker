11c3aff85d945d269e7e7c0763493c416ae08a8a
Zinhle0309 <72995433+Zinhle0309@users.noreply.github.com>
2023-06-28 22:36:09 +0200

    Add files via upload

---
+++ Budget Tracker/src/Components/Budget.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../Context/AppContext';
+
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: R{budget}</span>
+        </div>
+    );
+};
+
+export default Budget;
